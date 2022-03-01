13b5ac1f7dc0ca908c4d204469c35af44d7ccf05
Skull Merlin <86374920+skkuull@users.noreply.github.com>
2022-03-01 22:24:55 +0200

    symbols

--- src/client/game/symbols.hpp
+++ src/client/game/symbols.hpp
@@ -8,10 +8,17 @@ namespace game
 	 * Functions
 	 **************************************************************/
 
+	WEAK symbol<void(int type, VariableUnion u)> AddRefToValue{0x140315830, 0x14043C580}; // H1MP
+	WEAK symbol<void(int type, VariableUnion u)> RemoveRefToValue{0x140317340, 0x14043E090}; // H1MP
+	WEAK symbol<void(unsigned int id)> AddRefToObject{0, 0x14043C570 }; // H1MP
+	WEAK symbol<void(unsigned int id)> RemoveRefToObject{0, 0x14043DF80 }; // H1MP
+	WEAK symbol<unsigned int(unsigned int id)> AllocThread{0, 0x14043C8E0 }; // H1MP
+
 	WEAK symbol<void(int localClientNum, const char* text)> Cbuf_AddText{0x140342EB0, 0x1404033B0};
 	WEAK symbol<void(int localClientNum, int controllerIndex, const char* buffer,
 		void (int, int, const char*))> Cbuf_ExecuteBufferInternal{0, 0x1404034C0};
 	WEAK symbol<void(const char* message)> Conbuf_AppendText{0x1403E3300, 0x140513FF0};
+	WEAK symbol<char* (int start)> ConcatArgs{0x14021A7E0, 0x140335D70}; // h1mp
 	WEAK symbol<void(int localClientNum, int controllerIndex, const char* text)> Cmd_ExecuteSingleCommand{0x140343980, 0x140403F60};
 	WEAK symbol<void(const char* cmdName, void(), cmd_function_s* allocedCmd)> Cmd_AddCommandInternal{0x1403433E0, 0x140403950};
 	WEAK symbol<void(const char*)> Cmd_RemoveCommand{0x140343FF0, 0x1404045D0};
@@ -19,6 +26,8 @@ namespace game
 	WEAK symbol<void()> Cmd_EndTokenizeString{0x140343630, 0x140403C20};
 
 	WEAK symbol<void(void*, void*)> AimAssist_AddToTargetList{0, 0x14009D0F0};
+
+	WEAK symbol<void(unsigned int weapon, bool isAlternate, char* output, unsigned int maxStringLen)> BG_GetWeaponNameComplete{ 0x0, 0x140165580 };
 
 	WEAK symbol<void()> Com_Frame_Try_Block_Function{0, 0x1400D8310};
 	WEAK symbol<CodPlayMode()> Com_GetCurrentCoDPlayMode{0, 0x1405039A0};
@@ -53,6 +62,13 @@ namespace game
 	WEAK symbol<void(const char* gameName)> FS_Startup{0x1403B85D0, 0x1404EDD30};
 	WEAK symbol<void(const char* path, const char* dir)> FS_AddLocalizedGameDirectory{0x1403B6030, 0x1404EBE20};
 
+	WEAK symbol<unsigned int(unsigned int, unsigned int)> GetVariable{ 0x0, 0x1403F3730 };
+
+	WEAK symbol<void()> GScr_LoadConsts{0x1402D13E0, 0x140393810};
+	WEAK symbol<unsigned int(unsigned int parentId, unsigned int name)> FindVariable{0x1403165D0, 0x14043D430}; // H1MP
+	WEAK symbol<unsigned int(int entnum, unsigned int classnum)> FindEntityId{0x1403166D0, 0x14043D330}; // H1MP
+	WEAK symbol<void(unsigned int parentId, unsigned int index)> RemoveVariableValue{0, 0x14043E130}; // H1MP
+	WEAK symbol<void(VariableValue* result, unsigned int classnum, int entnum, int offset)> GetEntityFieldValue{0x14031AAD0, 0x140441780}; // H1MP
 	WEAK symbol<int(const char* fname)> generateHashValue{0x1400FE8A0, 0x1401B1010};
 
 	WEAK symbol<void()> G_Glass_Update{0, 0x14033A640};
@@ -88,9 +104,17 @@ namespace game
 #define R_AddCmdDrawTextWithCursor(TXT, MC, F, UNK, X, Y, XS, YS, R, C, S, CP, CC) \
 	H1_AddBaseDrawTextCmd(TXT, MC, F, game::R_GetFontHeight(F), X, Y, XS, YS, R, C, S, CP, CC, game::R_DrawSomething(S))
 
+	WEAK symbol<unsigned int(unsigned int localId, const char* pos, unsigned int paramcount)> VM_Execute{0x0, 0x140444350};
+
+	WEAK symbol<void(unsigned int id, scr_string_t stringValue, unsigned int paramcount)> Scr_NotifyId{
+	0x14031CB80, 0x1404437E0
+	};
+	WEAK symbol<const float* (const float* v)> Scr_AllocVector{0x140317D10, 0x14043E7D0}; // H1MP
 	WEAK symbol<float(int index)> Scr_GetFloat{0x140374D20, 0x140442D10};
 	WEAK symbol<const char*(int index)> Scr_GetString{0, 0x14032F0A0};
 	WEAK symbol<int()> Scr_GetNumParam{0x140374F30, 0x140442E70};
+	WEAK symbol<void()> Scr_ClearOutParams{0x14031B7C0, 0x140442510}; // H1MP
+	WEAK symbol<scr_entref_t(unsigned int entId)> Scr_GetEntityIdRef{ 0x14031A0D0, 0x1403F68A0 };
 
 	WEAK symbol<ScreenPlacement* ()> ScrPlace_GetViewPlacement{0x1401981F0, 0x140288550};
 
@@ -101,8 +125,12 @@ namespace game
 	WEAK symbol<bool(int clientNum, const char* menu)> Menu_IsMenuOpenAndVisible{0, 0x1404C7320};
 
 	WEAK symbol<scr_string_t(const char* str)> SL_FindString{0x140314AF0, 0x14043B470};
+	WEAK symbol<scr_string_t(const char* str, unsigned int user)> SL_GetString{0x140314D90, 0x14043B840}; // H1MP
+	WEAK symbol<const char* (scr_string_t stringValue)> SL_ConvertToString{0x14036D420, 0x14043B170};
+	WEAK symbol<int(unsigned int classnum, int entnum, int offset)> Scr_SetObjectField{ 0x14026B620, 0x140385330 }; // H1MP
 
 	WEAK symbol<void(netadr_s* from)> SV_DirectConnect{0, 0x140480860};
+	WEAK symbol<void(int arg, char* buffer, int bufferLength)> SV_Cmd_ArgvBuffer{ 0x1402EEFD0, 0x1403B05C0 };
 	WEAK symbol<void(const char* text_in)> SV_Cmd_TokenizeString{0x1402EF050, 0x140404D20};
 	WEAK symbol<void()> SV_Cmd_EndTokenizedString{0x140344700, 0x140404CE0};
 
@@ -140,17 +168,30 @@ namespace game
 
 	WEAK symbol<const char*(const char* string)> UI_SafeTranslateString{0x140350430, 0x14041C580};
 
+	WEAK symbol<void* (jmp_buf* Buf, int Value)> longjmp{ 0x140648FD4, 0x140779F64 };
+	WEAK symbol<int(jmp_buf* Buf)> _setjmp{ 0x1406BFD30, 0x1407F5F90 };
+
 	/***************************************************************
 	 * Variables
 	 **************************************************************/
 
 	WEAK symbol<CmdArgs> sv_cmd_args{0, 0x14946BA20};
 
+	WEAK symbol<int> g_script_error_level{0x14A1917A8, 0x14A33C824}; // H1MP
+	WEAK symbol<jmp_buf> g_script_error{0x14A1917B0, 0x14A33C940}; // H1MP
+	
+	WEAK symbol<unsigned int> levelEntityId{0x149AF55B0, 0x149CA0730}; // H1MP
+
 	WEAK symbol<const char*> command_whitelist{0x141079A60, 0x14120C360};
 	WEAK symbol<cmd_function_s*> cmd_functions{0x14AD99AB8, 0x14946BAC8};
 	WEAK symbol<CmdArgs> cmd_args{0x14AD99960, 0x14946B970};
 
 	WEAK symbol<int> g_poolSize{0, 0x140FEADF0};
+	WEAK symbol<scr_classStruct_t> g_classMap{ 0x14080A840, 0x1412106B0 };
+
+	WEAK symbol<scrVarGlob_t> scr_VarGlob{0x149B1D680, 0x149CC8800}; // H1MP
+	WEAK symbol<scrVmPub_t> scr_VmPub{0x14A1938C0, 0x14A33EA40}; // H1MP
+	WEAK symbol<function_stack_t> scr_function_stack{0x14A19DE40, 0x14A348FC0}; // H1MP
 
 	WEAK symbol<GfxDrawMethod_s> gfxDrawMethod{0, 0x14FD21180};
 
