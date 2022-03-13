73cea1553d8b8bb67478830594dd36a73761fdf3
Federico Cecchetto <fedecek3@gmail.com>
2022-03-13 15:27:02 +0100

    Revert "Fix"
    This reverts commit 39fa6db1fb9e9e50c91a5be06be24696f8fc2ea3.

--- src/client/component/game_console.hpp
+++ src/client/component/game_console.hpp
@@ -1,7 +1,11 @@
 #pragma once
+#include "game/dvars.hpp"
 
 namespace game_console
 {
 	bool console_char_event(int local_client_num, int key);
 	bool console_key_event(int local_client_num, int key, int down);
+
+	bool match_compare(const std::string& input, const std::string& text, const bool exact);
+	void find_matches(std::string input, std::vector<dvars::dvar_info>& suggestions, const bool exact);
 }
