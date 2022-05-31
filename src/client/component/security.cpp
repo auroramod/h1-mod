c8adef5541c49055b72da64138b2df3b6f7b6507
Federico Cecchetto <fedecek3@gmail.com>
2022-06-01 01:06:41 +0200

    Security component

---
+++ src/client/component/security.cpp
@@ -0,0 +1,37 @@
+#include <std_include.hpp>
+#include "loader/component_loader.hpp"
+
+#include "game/game.hpp"
+
+#include <utils/hook.hpp>
+
+namespace security
+{
+	namespace
+	{
+		void set_cached_playerdata_stub(const int localclient, const int index1, const int index2)
+		{
+			if (index1 >= 0 && index1 < 18 && index2 >= 0 && index2 < 42)
+			{
+				utils::hook::invoke<void>(0x61A9D_b, localclient, index1, index2);
+			}
+		}
+	}
+
+	class component final : public component_interface
+	{
+	public:
+		void post_unpack() override
+		{
+			if (game::environment::is_sp())
+			{
+				return;
+			}
+
+			// Patch vulnerability in PlayerCards_SetCachedPlayerData
+			utils::hook::call(0xF4632_b, set_cached_playerdata_stub);
+		}
+	};
+}
+
+REGISTER_COMPONENT(security::component)
