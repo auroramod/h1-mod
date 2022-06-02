d6f161fd21cb3f2f385b2d581933005fa4af9009
Federico Cecchetto <fedecek3@gmail.com>
2022-06-03 01:47:03 +0200

    Allow team change

--- data/ui_scripts/patches/__init__.lua
+++ data/ui_scripts/patches/__init__.lua
@@ -7,3 +7,7 @@ function GetGameModeName()
 	return Engine.Localize(Engine.TableLookup(GameTypesTable.File, 
 		GameTypesTable.Cols.Ref, GameX.GetGameMode(), GameTypesTable.Cols.Name))
 end
+
+function NeverAllowChangeTeams()
+	return false
+end
