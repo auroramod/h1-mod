8bfa958fe7936317ee1100f73e2bac1dfa3ef5d6
Skull <86374920+skkuull@users.noreply.github.com>
2022-06-12 13:56:17 +0300

    Update server_list.cpp [skip ci]

--- src/client/component/server_list.cpp
+++ src/client/component/server_list.cpp
@@ -300,7 +300,7 @@ namespace server_list
 
 	bool get_master_server(game::netadr_s& address)
 	{
-		return game::NET_StringToAdr("master.ff.h1p.co:20810", &address);
+		return game::NET_StringToAdr("master.h1.gg:20810", &address);
 	}
 
 	void handle_info_response(const game::netadr_s& address, const utils::info_string& info)
