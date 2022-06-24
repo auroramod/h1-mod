18abb5f338144785b939686278b2443559bd8de1
mjkzy <mjkzyalt@gmail.com>
2022-06-24 07:55:08 -0500

    add extra gamemodes to UI

---
+++ data/ui_scripts/extra_gamemodes/__init__.lua
@@ -0,0 +1,11 @@
+Cac.GameModes.Data = {
+    Standard = {
+        Label = Engine.Localize("@MPUI_STANDARD_CAPS"),
+        Image = "h1_ui_icon_playlist_standard",
+        List = 
+        {
+            "dm", "war", "sd", "dom", "conf", "sab", "koth", "hp", "gun", 
+            "dd", "ctf" -- missing gamemodes from UI
+        }
+    }
+}
