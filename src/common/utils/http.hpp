ed7decf8a9949879c229e9796fb522158dfd81d0
Federico Cecchetto <fedecek3@gmail.com>
2022-06-26 23:07:25 +0200

    Use curl

--- src/common/utils/http.hpp
+++ src/common/utils/http.hpp
@@ -3,9 +3,18 @@
 #include <string>
 #include <optional>
 #include <future>
+#include <gsl/gsl>
+
+#include <curl/curl.h>
 
 namespace utils::http
 {
-	std::optional<std::string> get_data(const std::string& url);
-	std::future<std::optional<std::string>> get_data_async(const std::string& url);
+	struct result
+	{
+		CURLcode code;
+		std::string buffer;
+	};
+
+	std::optional<result> get_data(const std::string& url);
+	std::future<std::optional<result>> get_data_async(const std::string& url);
 }
