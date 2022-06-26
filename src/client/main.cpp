6a069df3b42a0d841669ca101f20e1c0f00dc180
mjkzy <mjkzyalt@gmail.com>
2022-06-26 16:56:59 -0500

    get rid of useless function

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -181,16 +181,6 @@ void limit_parallel_dll_loading()
 	RegCloseKey(key);
 }
 
-// solution for other processes that may launch the mod
-void apply_proper_directory()
-{
-	char module_path[MAX_PATH];
-	GetModuleFileNameA(nullptr, module_path, MAX_PATH);
-	PathRemoveFileSpecA(module_path);
-	SetCurrentDirectoryA(module_path);
-	SetDllDirectoryA(module_path);
-}
-
 int main()
 {
 	ShowWindow(GetConsoleWindow(), SW_HIDE);
@@ -217,8 +207,6 @@ int main()
 
 		try
 		{
-			//apply_proper_directory();
-
 			if (!component_loader::post_start()) return 0;
 
 			auto mode = detect_mode_from_arguments();
