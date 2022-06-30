9a80d73f7cd4fbe4d3c1df06256931ecf0f3e43f
mjkzy <mjkzyalt@gmail.com>
2022-06-30 12:15:49 -0500

    use iw5-script's http methods

--- src/common/utils/http.cpp
+++ src/common/utils/http.cpp
@@ -6,6 +6,32 @@ namespace utils::http
 {
 	namespace
 	{
+		struct progress_helper
+		{
+			const std::function<void(size_t)>* callback{};
+			std::exception_ptr exception{};
+		};
+
+		int progress_callback(void* clientp, const curl_off_t /*dltotal*/, const curl_off_t dlnow, const curl_off_t /*ultotal*/, const curl_off_t /*ulnow*/)
+		{
+			auto* helper = static_cast<progress_helper*>(clientp);
+
+			try
+			{
+				if (*helper->callback)
+				{
+					(*helper->callback)(dlnow);
+				}
+			}
+			catch (...)
+			{
+				helper->exception = std::current_exception();
+				return -1;
+			}
+
+			return 0;
+		}
+
 		size_t write_callback(void* contents, const size_t size, const size_t nmemb, void* userp)
 		{
 			auto* buffer = static_cast<std::string*>(userp);
@@ -16,7 +42,8 @@ namespace utils::http
 		}
 	}
 
-	std::optional<result> get_data(const std::string& url)
+	std::optional<result> get_data(const std::string& url, const std::string& fields,
+		const headers& headers, const std::function<void(size_t)>& callback)
 	{
 		curl_slist* header_list = nullptr;
 		auto* curl = curl_easy_init();
@@ -31,13 +58,28 @@ namespace utils::http
 			curl_easy_cleanup(curl);
 		});
 
+		for (const auto& header : headers)
+		{
+			auto data = header.first + ": " + header.second;
+			header_list = curl_slist_append(header_list, data.data());
+		}
+
 		std::string buffer{};
+		progress_helper helper{};
+		helper.callback = &callback;
 
 		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, header_list);
 		curl_easy_setopt(curl, CURLOPT_URL, url.data());
 		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
 		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
-		curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1);
+		curl_easy_setopt(curl, CURLOPT_XFERINFOFUNCTION, progress_callback);
+		curl_easy_setopt(curl, CURLOPT_XFERINFODATA, &helper);
+		curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0);
+
+		if (!fields.empty())
+		{
+			curl_easy_setopt(curl, CURLOPT_POSTFIELDS, fields.data());
+		}
 
 		const auto code = curl_easy_perform(curl);
 
@@ -49,20 +91,24 @@ namespace utils::http
 
 			return result;
 		}
-		else
-		{
-			result result;
-			result.code = code;
 
-			return result;
+		if (helper.exception)
+		{
+			std::rethrow_exception(helper.exception);
 		}
+
+		result result;
+		result.code = code;
+
+		return result;
 	}
 
-	std::future<std::optional<result>> get_data_async(const std::string& url)
+	std::future<std::optional<result>> get_data_async(const std::string& url, const std::string& fields,
+		const headers& headers, const std::function<void(size_t)>& callback)
 	{
-		return std::async(std::launch::async, [url]()
+		return std::async(std::launch::async, [url, fields, headers, callback]()
 		{
-			return get_data(url);
+			return get_data(url, fields, headers, callback);
 		});
 	}
 }
