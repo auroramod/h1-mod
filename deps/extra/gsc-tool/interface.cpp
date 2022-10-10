a04f90858a637c66fb8d69e32f4e51ce1ee32750
m <mjkzyalt@gmail.com>
2022-10-10 13:21:14 -0500

    allow custom scripts to include stock scripts

--- deps/extra/gsc-tool/interface.cpp
+++ deps/extra/gsc-tool/interface.cpp
@@ -13,8 +13,18 @@ namespace gsc
 		return compiler;
 	}
 
+	std::unique_ptr<xsk::gsc::decompiler> decompiler()
+	{
+		return std::make_unique<xsk::gsc::h1::decompiler>();
+	}
+
 	std::unique_ptr<xsk::gsc::assembler> assembler()
 	{
 		return std::make_unique<xsk::gsc::h1::assembler>();
+	}
+
+	std::unique_ptr<xsk::gsc::disassembler> disassembler()
+	{
+		return std::make_unique<xsk::gsc::h1::disassembler>();
 	}
 }
--- deps/extra/gsc-tool/interface.hpp
+++ deps/extra/gsc-tool/interface.hpp
@@ -3,5 +3,7 @@
 namespace gsc
 {
 	std::unique_ptr<xsk::gsc::compiler> compiler();
+	std::unique_ptr<xsk::gsc::decompiler> decompiler();
 	std::unique_ptr<xsk::gsc::assembler> assembler();
+	std::unique_ptr<xsk::gsc::disassembler> disassembler();
 }
