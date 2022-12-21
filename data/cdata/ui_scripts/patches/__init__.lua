163de98ad5505f552acd9d71adbd6afb61859d86
Vlad <yobabay23@gmail.com>
2022-12-21 21:31:29 +0300

    Background effects tweak (#394)
    * Background effects tweak
    
    * Small fix
    
    * Revert: 'language' deletion (4542b97)
    
    * cleanup + readd vignette

--- data/cdata/ui_scripts/patches/__init__.lua
+++ data/cdata/ui_scripts/patches/__init__.lua
@@ -1,23 +1,26 @@
+require("language")
+require("background_effects")
+
 if (game:issingleplayer()) then
     require("sp_unlockall")
     return
 end
 
 if (Engine.InFrontend()) then
-	require("shader_dialog")
-	require("gamemodes")
-	require("no_mode_switch")
-	require("disable_useless_things")
+    require("shader_dialog")
+    require("gamemodes")
+    require("no_mode_switch")
+    require("disable_useless_things")
 else
-	require("scoreboard")
+    require("scoreboard")
 end
 
 -- defined in mp_hud/hudutils.lua
 function GetGameModeName()
-	return Engine.Localize(Engine.TableLookup(GameTypesTable.File, 
-		GameTypesTable.Cols.Ref, GameX.GetGameMode(), GameTypesTable.Cols.Name))
+    return Engine.Localize(Engine.TableLookup(GameTypesTable.File, GameTypesTable.Cols.Ref, GameX.GetGameMode(),
+        GameTypesTable.Cols.Name))
 end
 
 function NeverAllowChangeTeams()
-	return false
+    return false
 end
