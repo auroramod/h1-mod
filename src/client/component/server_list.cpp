123c67e34eff3065bedf0ff862a767e05e587445
mjkzy <m@mikzy.me>
2026-09-22 00:48:38 -0500

    handle fragmented responses with EOT
    closes #851

--- src/client/component/server_list.cpp
+++ src/client/component/server_list.cpp
@@ -46,6 +46,7 @@ namespace server_list
 		{
 			game::netadr_s address{};
 			volatile bool requesting = false;
+			int request_time = 0;
 			std::unordered_map<game::netadr_s, int> queued_servers{};
 		} master_state;
 
@@ -84,6 +85,7 @@ namespace server_list
 			if (get_master_server(master_state.address))
 			{
 				master_state.requesting = true;
+				master_state.request_time = game::Sys_Milliseconds();
 
 				network::send(master_state.address, "getservers", utils::string::va("H1 %i full empty", PROTOCOL));
 			}
@@ -216,6 +218,11 @@ namespace server_list
 
 		void do_frame_work()
 		{
+			if (master_state.requesting && game::Sys_Milliseconds() - master_state.request_time > 5'000)
+			{
+				master_state.requesting = false;
+			}
+
 			auto& queue = master_state.queued_servers;
 			if (queue.empty())
 			{
@@ -526,7 +533,10 @@ namespace server_list
 						return;
 					}
 
-					master_state.requesting = false;
+					if (data.find("\\EOT") != std::string::npos)
+					{
+						master_state.requesting = false;
+					}
 
 					std::optional<size_t> start{};
 					for (std::size_t i = 0; i + 6 < data.size(); ++i)
