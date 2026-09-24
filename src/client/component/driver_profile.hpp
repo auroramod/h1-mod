632341ba248ad8bb81f8971328580cb9a62d030e
m <mjkzyalt@gmail.com>
2026-09-24 13:34:12 -0500

    remove comment

--- src/client/component/driver_profile.hpp
+++ src/client/component/driver_profile.hpp
@@ -2,7 +2,6 @@
 
 namespace driver_profile
 {
-	// returns true and fills filename if the caller is an AMD driver module asking for the main executable name
 	bool get_module_file_name(void* return_address, HMODULE module, LPSTR filename, DWORD size, DWORD* result);
 	bool get_module_file_name(void* return_address, HMODULE module, LPWSTR filename, DWORD size, DWORD* result);
 }
